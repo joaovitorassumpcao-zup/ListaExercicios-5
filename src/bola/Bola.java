b2729e4046607af619300d1185fc18673365bf56
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-06-12 19:44:57 -0300

    Bola

--- src/bola/Bola.java
+++ src/bola/Bola.java
@@ -1,4 +1,26 @@
 package bola;
 
+
 public class Bola {
+    public float circunferencia;
+    public float velocidade;
+    public String cor;
+    public String marca;
+    public String material;
+
+    public Bola() {
+        circunferencia = 20;
+        velocidade = 0;
+        cor = "vermelho";
+        marca = "Nike";
+        material = "Borracha";
+    }
+
+    public String mostraCor() {
+        return cor;
+    }
+
+    public void trocaCor(String cor) {
+        this.cor = cor;
+    }
 }
--- src/bola/Main.java
+++ src/bola/Main.java
@@ -1,4 +1,13 @@
 package bola;
 
 public class Main {
+    public static void main(String[] args) {
+        Bola bola = new Bola();
+
+        System.out.println("Cor da bola é " + bola.mostraCor());
+
+        bola.trocaCor("Azul");
+
+        System.out.println("Cor da bola após a troca é " + bola.mostraCor());
+    }
 }
